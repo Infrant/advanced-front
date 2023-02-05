e251a31268a593568e210bfc318c08f86cc6d384
Yury Glushko <glushko@nanosemantics.ai>
2023-02-06 00:30:24 +0300

    feat(01/05): added MiniCssExtractPlugin, scss modules

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,10 +1,10 @@
 import React, {useState} from 'react';
-import './Counter.scss'
+import style from './Counter.module.scss'
 
 export const Counter = () => {
   const [count, setCount] = useState(0)
   return (
-    <h1 onClick={() => setCount(prev => prev + 1)}>
+    <h1 className={style.counter} onClick={() => setCount(prev => prev + 1)}>
       {count}
     </h1>
   );
