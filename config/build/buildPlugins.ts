a3d1af4f8affa164e148a54dbfa539ca7ed8d0a4
Infrant <yury.glushko.web@gmail.com>
2023-02-06 00:30:24 +0300

    feat(01/05): added MiniCssExtractPlugin, scss modules

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,17 @@
 import webpack, {WebpackPluginInstance} from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import {BuildOptions} from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildPlugins({paths}: BuildOptions): WebpackPluginInstance[] {
   return [
     new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({template: paths.html}),
+    new HtmlWebpackPlugin({
+      template: paths.html
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[name].[contenthash].css',
+    })
   ]
 }
