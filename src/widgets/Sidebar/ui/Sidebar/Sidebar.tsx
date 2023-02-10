7d1a7389e43936223be50d904cac8fb934cc766c
Yury Glushko <glushko@nanosemantics.ai>
2023-02-10 22:46:54 +0300

    feat(02/04): added Sidebar, App Layout

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,26 @@
+import React, {FC, useState} from "react";
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Sidebar.module.scss'
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+
+interface SidebarProps {
+  className?: string
+}
+
+export const Sidebar: FC<SidebarProps> = ({className}) => {
+  const [collapsed, setCollapsed] = useState(false)
+
+  const onToggle = () => setCollapsed(prevState => !prevState)
+
+  return (
+    <div className={classNames(cls.sidebar, {[cls.collapsed]: collapsed}, [className])}
+    >
+      <button onClick={onToggle}>
+        toggle
+      </button>
+      <div className={cls.switchers}>
+        <ThemeSwitcher/>
+      </div>
+    </div>
+  );
+};
