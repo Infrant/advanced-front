071ef4bee950251c3b61f2ee8fd688f5c6a33de6
Infrant <yury.glushko.web@gmail.com>
2023-02-11 21:43:20 +0300

    feat(02/010): added jest, classNames.test.ts

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -10,7 +10,9 @@ interface NavbarProps {
 export const Navbar:FC<NavbarProps> = ({className}) => (
   <div className={classNames(cls.Navbar, {}, [className])}>
     <div className={cls.links}>
+      {/* eslint-disable-next-line i18next/no-literal-string */}
       <AppLink theme={AppLinkTheme.secondary} to='/about'>About</AppLink>
+      {/* eslint-disable-next-line i18next/no-literal-string */}
       <AppLink theme={AppLinkTheme.secondary} to='/'>Main</AppLink>
     </div>
   </div>
