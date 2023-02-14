d978623da00e1730ee097711e529a07d37f76f3c
Infrant <yury.glushko.web@gmail.com>
2023-02-15 00:29:25 +0300

    feat(03/01): added ErrorBoundary

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -11,7 +11,7 @@ export const NotFoundPage: FC<NotFoundPageProps> = ({className}) => {
   const {t} = useTranslation()
   return (
     <div className={classNames(cls.notFoundPage, {}, [className])}>
-      {t('pageNotFound')}
+      {t('page_no_found')}
     </div>
   );
 };
