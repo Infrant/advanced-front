560e309ce8d840957e13aeabb6cd59da863bf889
Yury Glushko <glushko@nanosemantics.ai>
2023-02-18 20:27:52 +0300

    feat(03/06): fixed ts lint

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import {classNames} from 'shared/lib/classNames/classNames';
 import {Button} from 'shared/ui/Button/Button';
 import {ThemeSwitcher} from 'shared/ui/ThemeSwitcher/ThemeSwitcher';
 import {LangSwitcher} from 'shared/ui/LangSwitcher/LangSwitcher';
+import {useTranslation} from 'react-i18next';
 import cls from './Sidebar.module.scss'
 
 interface SidebarProps {
@@ -11,6 +12,7 @@ interface SidebarProps {
 
 export const Sidebar: FC<SidebarProps> = ({className}) => {
   const [collapsed, setCollapsed] = useState(false)
+  const {t} = useTranslation()
 
   const onToggle = () => setCollapsed((prevState) => !prevState)
 
@@ -27,7 +29,7 @@ export const Sidebar: FC<SidebarProps> = ({className}) => {
         data-testid='sidebar-toggle'
         onClick={onToggle}
       >
-        toggle
+        {t('toggle')}
       </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
