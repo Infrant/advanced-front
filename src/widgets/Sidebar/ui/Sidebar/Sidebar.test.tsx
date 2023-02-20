1716b5acbceb7dc6703f45da15cc4c9d7aab2d1a
Yury Glushko <glushko@nanosemantics.ai>
2023-02-20 19:13:10 +0300

    fix(03/07): fix unit tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,16 @@
 import {fireEvent, render, screen} from '@testing-library/react';
 import {Sidebar} from 'widgets/Sidebar';
-import {renderWithTranslation} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import {renderWithTranslation} from 'shared/lib/tests/renderWithTranslation';
+import {ComponentRender} from 'shared/lib/tests/ComponentRender';
 
 describe('Sidebar', () => {
   test('render Sidebar', () => {
-    renderWithTranslation(<Sidebar />)
+    ComponentRender(<Sidebar />)
     expect(screen.getByTestId('sidebar')).toBeInTheDocument()
   })
 
   test('render Sidebar', () => {
-    renderWithTranslation(<Sidebar />)
+    ComponentRender(<Sidebar />)
     const toggleBtn = screen.getByTestId('sidebar-toggle')
     fireEvent.click(toggleBtn)
     expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
