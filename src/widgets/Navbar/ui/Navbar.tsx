c4e7dcba517f7db6e02eea7a70b5ea59d489d570
Yury Glushko <glushko@nanosemantics.ai>
2023-02-21 20:59:52 +0300

    feat(04/01): added modal, portal, changed navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,8 @@
-import React, {FC} from 'react';
+import React, {FC, useCallback, useState} from 'react';
 import {classNames} from 'shared/lib/classNames/classNames';
-import {AppLink, AppLinkTheme} from 'shared/ui/AppLink/AppLink';
 import {useTranslation} from 'react-i18next';
+import {Modal} from 'shared/ui/Modal/Modal';
+import {Button, ButtonVariant} from 'shared/ui/Button/Button';
 import cls from './Navbar.module.scss'
 
 interface NavbarProps {
@@ -10,12 +11,26 @@ interface NavbarProps {
 
 export const Navbar:FC<NavbarProps> = ({className}) => {
   const {t} = useTranslation()
+  const [isOpen, setIsOpen] = useState(false)
+
+  const onToggleModal = useCallback(() => {
+    setIsOpen((prev) => !prev)
+  }, [])
 
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <div className={cls.links}>
-        /
-      </div>
+      <Button
+        variant={ButtonVariant.CLEAR_INVERTED}
+        className={cls.links}
+        onClick={onToggleModal}
+      >
+        {t('sign_in')}
+      </Button>
+      <Modal isOpen={isOpen} onClose={onToggleModal}>
+        {/* eslint-disable-next-line i18next/no-literal-string */}
+        {/* eslint-disable-next-line max-len */}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perferendis, vitae. Alias architecto beatae consequuntur cum delectus deleniti dignissimos ducimus, error facere labore magni maxime modi odit optio perferendis provident quae quasi quidem quis sed sint sit suscipit ullam ut velit veritatis voluptates! Hic quaerat, rem. Asperiores blanditiis consectetur culpa deleniti exercitationem libero molestias obcaecati optio sapiente veniam! Dolores ea eligendi esse hic provident, quam quia quibusdam quisquam ratione tempore tenetur, veniam. Dolorum eveniet id iste reiciendis sunt. Ab beatae consequatur corporis dicta est excepturi harum laboriosam magnam minima modi officiis, quae quam ratione reiciendis repudiandae sapiente sequi tempora temporibus?
+      </Modal>
     </div>
   )
 }
