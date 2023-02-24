001c499628ceafdae5b9785f2c765d770a701dac
Infrant <yury.glushko.web@gmail.com>
2023-02-24 19:25:18 +0300

    feat(04/02): added redux, entity Counter

--- src/shared/lib/tests/ComponentRender.tsx
+++ src/shared/lib/tests/ComponentRender.tsx
@@ -3,20 +3,26 @@ import {render} from '@testing-library/react';
 import {MemoryRouter} from 'react-router-dom';
 import {I18nextProvider} from 'react-i18next';
 import i18nTest from 'shared/config/i18n/i18nTest';
+import {StateSchema, StoreProvider} from 'app/providers/StoreProvider';
+import {DeepPartial} from '@reduxjs/toolkit';
 
 interface ComponentRenderOptions {
   route?: string
+  initialState?: DeepPartial<StateSchema>
 }
 
 export function ComponentRender(component: ReactNode, options: ComponentRenderOptions = {}) {
   const {
     route = '/',
+    initialState,
   } = options
   return render(
-    <MemoryRouter initialEntries={[route]}>
-      <I18nextProvider i18n={i18nTest}>
-        {component}
-      </I18nextProvider>
-    </MemoryRouter>,
+    <StoreProvider initialState={initialState}>
+      <MemoryRouter initialEntries={[route]}>
+        <I18nextProvider i18n={i18nTest}>
+          {component}
+        </I18nextProvider>
+      </MemoryRouter>
+    </StoreProvider>,
   )
 }
