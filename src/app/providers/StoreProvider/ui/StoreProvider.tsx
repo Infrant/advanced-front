7a1887448eeaebf8372a8a2fefdbda0177937fb9
Yury Glushko <glushko@nanosemantics.ai>
2023-03-05 00:15:35 +0300

    feat(05/04): added instance api, env url, added instance to thunk extra

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -2,6 +2,7 @@ import {FC} from 'react';
 import {Provider} from 'react-redux';
 import {createReduxStore} from 'app/providers/StoreProvider';
 import {DeepPartial, ReducersMapObject} from '@reduxjs/toolkit';
+import {useNavigate} from 'react-router-dom';
 import {StateSchema} from '../config/StateSchema';
 
 interface StoreProviderProps {
@@ -11,9 +12,13 @@ interface StoreProviderProps {
 
 export const StoreProvider: FC<StoreProviderProps> = (props) => {
   const {children, initialState, asyncReducers} = props
+
+  const navigate = useNavigate()
+
   const store = createReduxStore(
     initialState as StateSchema,
     asyncReducers as ReducersMapObject,
+    navigate,
   )
 
   return (
