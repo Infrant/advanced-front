e1539170e30f265445f0c39e504d55c1d43b73aa
Yury Glushko <glushko@nanosemantics.ai>
2023-03-05 16:50:17 +0300

    feat(05/05): added ts strict mode, profile entity with fetch data, slice etc

---
+++ src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
@@ -0,0 +1,16 @@
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import {ThunkConfig} from 'app/providers/StoreProvider/config/StateSchema';
+import {Profile} from '../../types/profile';
+
+export const fetchProfileData = createAsyncThunk<Profile, void, ThunkConfig<string>>(
+  'profile/fetchProfileData',
+  async (_, {rejectWithValue, extra}) => {
+    try {
+      const response = await extra.api.get<Profile>('/profile')
+
+      return response.data
+    } catch (e) {
+      return rejectWithValue('wrong_login')
+    }
+  },
+)
--- src/entities/Profile/model/slice/profileSlice.ts
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -1,5 +1,7 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit'
-import {ProfileSchema} from '../types/profile';
+import {loginByUserName} from 'featrures/AuthByUserName/model/services/loginByUserName/loginByUserName';
+import {Profile, ProfileSchema} from '../types/profile';
+import {fetchProfileData} from '../services/fetchProfileData/fetchProfileData';
 
 const initialState: ProfileSchema = {
   readonly: true,
@@ -12,6 +14,21 @@ export const profileSlice = createSlice({
   name: 'profile',
   initialState,
   reducers: {
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProfileData.pending, (state) => {
+        state.error = null
+        state.isLoading = true
+      })
+      .addCase(fetchProfileData.fulfilled, (state, action: PayloadAction<Profile>) => {
+        state.isLoading = false
+        state.data = action.payload
+      })
+      .addCase(fetchProfileData.rejected, (state, action) => {
+        state.error = action.payload
+        state.isLoading = false
+      })
   },
 })
 
